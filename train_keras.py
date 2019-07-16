742501c419ce48732f8963e53ca45a0a8abad0a5
Cyrus Tau <cyrus.tau@gmail.com>
2019-07-15 18:50:36 -0700

    Change model test

--- train_keras.py
+++ train_keras.py
@@ -57,9 +57,9 @@ y = np.divide(y,200)
 batch_size = 50
 dropout = .4
 model = Sequential()
-model.add(l.Conv2D(128,activation="relu",kernel_size=(3,3),input_shape=(60,80,1),data_format="channels_last"))
-model.add(l.Conv2D(128,activation="relu",kernel_size=(3,3),data_format="channels_last"))
-model.add(l.Conv2D(128,activation="relu",kernel_size=(3,3),data_format="channels_last"))
+model.add(l.Conv2D(128,activation="sigmoid",kernel_size=(3,3),input_shape=(60,80,1),data_format="channels_last"))
+model.add(l.Conv2D(128,activation="sigmoid",kernel_size=(3,3),data_format="channels_last"))
+model.add(l.Conv2D(128,activation="sigmoid",kernel_size=(3,3),data_format="channels_last"))
 model.add(l.Flatten())
 # model.add(l.Reshape((60,80)))
 # model.add(l.SimpleRNN(100,activation="relu",dropout=dropout,recurrent_dropout=dropout))
